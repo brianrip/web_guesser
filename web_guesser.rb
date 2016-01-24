1e67e8dfc1766b1fe4947e17b0656c63fd7ac65a
Brian Rippeto <rippeto@gmail.com>
2016-01-24 11:58:25 -0700

    I3 refactor complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,27 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-secret = rand(100)
+random_num ||= rand(100)
 
 get '/' do
-  erb :index, :locals => {:secret => secret}
+	guess = params["guess"]
+	message = guess_checker(guess, random_num)
+	erb :index, :locals => {:message => message}
+end
+
+def guess_checker(guess, random_num)
+  return "take a guess" unless guess != nil
+  guess = guess.to_i
+  case
+    when guess > (random_num + 5)
+      "Way too high!"
+    when guess > random_num
+      "Too high"
+    when guess < (random_num - 5)
+      "Way too low"
+    when guess < random_num
+      "Too low"
+    else
+      "Correct! The secret number was #{random_num}"
+    end
 end
